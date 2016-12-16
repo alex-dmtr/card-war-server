26515b558685cfbac4ff017b2b4d00567946c55f
Alex Dumitru <alexadrian.dmtr@gmail.com>
2016-12-16 16:41:33 +0000

    Add welcome page

--- index.php
+++ index.php
@@ -10,30 +10,39 @@ password=75acd52173ec2333583d9d0ba0d46577eaa8bd58b44bae2de99aa5aafe89d0d4")
     or die('Could not connect: ' . pg_last_error());
     
     
-    $query = 'SELECT * FROM ' . $_GET['table'];
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
+    if (isset($_GET['table'])) {
+        $query = 'SELECT * FROM ' . $_GET['table'];
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
     
-    // echo "<table>\n";
-    
-    $json = array();
-    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-        $json[] = $line;
-        // echo "\t<tr>\n";
-        // foreach ($line as $col_value) {
-            // echo "\t\t<td>$col_value</td>\n";
-        // }
-        // echo "\t</tr>\n";
-    }
-    // echo "</table>\n";
-    
-
         
-    echo json_encode($json);
-    // Free resultset
-    pg_free_result($result);
+        // echo "<table>\n";
+        
+        $json = array();
+        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            $json[] = $line;
+            // echo "\t<tr>\n";
+            // foreach ($line as $col_value) {
+                // echo "\t\t<td>$col_value</td>\n";
+            // }
+            // echo "\t</tr>\n";
+        }
+        // echo "</table>\n";
+        
     
-    // Closing connection
-    pg_close($dbconn);
+            
+        echo json_encode($json);
+        // Free resultset
+        pg_free_result($result);
+        
+        // Closing connection
+        pg_close($dbconn);
+    
+    }
+    
+    else
+    
+    {
+        require 'home.html';
+    }
     // echo "Hello, world!";
 ?>
